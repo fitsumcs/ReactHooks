600fa7d4cbc247196411f263c41cedd1248706c6
Fitsum A <fitsumcs@gmail.com>
2021-04-29 00:53:57 +0300

    adding states on the buttons

--- use_effect/src/App.js
+++ use_effect/src/App.js
@@ -1,11 +1,21 @@
 
+import {useState} from 'react';
 function App() {
+  const [resType , setResType] = useState("posts")
   return (
-    <div className="App">
+    <div className="container mt-5">
       
-      <h1>Testing useEffect</h1>
-       
+    <div className="row bg-dark">
+     <div className="col-12 mt-2 p-2 ">
+     <button className="btn btn-lg btn-info m-2" onClick={()=>{setResType("posts")}}>Posts</button>
+     <button className="btn btn-lg btn-info m-2" onClick={()=>{setResType("comments")}}>Comments</button>
+     <button className="btn btn-lg btn-info m-2" onClick={()=>{setResType("users")}}>Users</button>
+     </div>
+     <div className="col-4">
+     <h3 className="text-light m-3">{resType}</h3>
+     </div>
     </div>
+  </div>
   );
 }
 
