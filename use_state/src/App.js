b1076b62407f64c85d120dd067e13c9377b92f8a
Fitsum A <fitsumcs@gmail.com>
2021-04-28 18:03:01 +0400

    Working with useState

--- use_state/src/App.js
+++ use_state/src/App.js
@@ -1,16 +1,27 @@
+import React , {useState} from 'react'
 
 function App() {
+
+    const [count, setCount] = useState(0);    
+
+    function increment(){
+      setCount(prev => prev+1)
+    }
+    function decrement(){
+      setCount(prev => prev-1)
+    }
+
   return (
     <div className="container mt-5">
       <div className="row bg-dark">
        <div className="col-4 ">
-       <button className="btn btn-lg btn-info">-</button>
+       <button className="btn btn-lg btn-info px-5 m-3" onClick={ decrement}>-</button>
        </div>
        <div className="col-4">
-       <p className="text-danger">Number</p>
+       <h3 className="text-light m-3">{count}</h3>
        </div>
        <div className="col-4">
-       <button className="btn btn-lg btn-info">+</button>
+       <button className="btn btn-lg btn-info px-5 m-3" onClick={increment}>+</button>
        </div>
        
       </div>
